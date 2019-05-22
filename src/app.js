033857e2e073cf6dcaf1306a6ee24916cf3fe5e4
torsami <torsami77@gmail.com>
2019-05-22 05:12:42 +0100

    [#166108765] Preparing node environment

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(express.static('ui'));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./ui/index.html'));
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server running on port ${PORT}`);
+});
+
+export default { app };
