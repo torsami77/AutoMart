fce6c4c4ff70d38de1155c748261bb4aafc0324a
torsami <torsami77@gmail.com>
2019-07-12 08:25:20 +0100

    feature (admin-activities) Persist Data for Admin users
    Verifies admin users and responds with retrospective data upon validation
    
    [Finishes #166760443, #166760521, #166978072, #166760712]

--- build/app.js
+++ build/app.js
@@ -43,7 +43,7 @@ const {
 
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 };
--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const { upload } = cloudUpload;
 
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 };
