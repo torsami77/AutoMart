49855669a5d687a57aa0193691eb4079bcb88f7a
torsami <torsami77@gmail.com>
2019-07-16 11:20:42 +0100

    ch(autograder-issues)troubleshooting v27
    HTTP Status Code issues
    
    [#167299032]

--- src/ctl/seller.js
+++ src/ctl/seller.js
@@ -216,7 +216,7 @@ class Seller {
   }
 
   static updatePrice(req, res) {
-    console.log(typeof (req.body.price));
+    console.log(typeof (parseFloat(req.body.price)));
     if (isNaN(parseFloat(req.body.price))) {
       res.status(400).send({
         status: 400,
